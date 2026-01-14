5d010ef3add40432338acdf4791ad3e68717e8b5
dejenegizawkidane <105314121+dejenegizawkidane@users.noreply.github.com>
2026-01-14 09:58:05 +0100

    Add files via upload

---
+++ Rcode_class2.R
@@ -0,0 +1,7 @@
+
+# let us take the data from the text book 
+rm(list = ls())
+library(tidyverse)
+browseURL("http://www.principlesofeconometrics.com/poe5/data/def/usmacro.def")
+load(url("http://www.principlesofeconometrics.com/poe5/data/rdata/usmacro.rdata"))
+
